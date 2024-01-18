700872f2561324c4453b1db98a2c9ae38621e48a
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2024-01-17 19:50:25 -0500

    Assignment 0 Done

--- assignments/assignment0/main.cpp
+++ assignments/assignment0/main.cpp
@@ -2,6 +2,12 @@
 #include <math.h>
 
 #include <ew/external/glad.h>
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/camera.h>
+#include <ew/transform.h>
+#include <ew/cameraController.h>
+#include <ew/texture.h>
 
 #include <GLFW/glfw3.h>
 #include <imgui.h>
@@ -12,40 +18,120 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 GLFWwindow* initWindow(const char* title, int width, int height);
 void drawUI();
 
+// Camera
+ew::CameraController cameraController;
+ew::Camera camera;
+void resetCamera();
+
 //Global state
 int screenWidth = 1080;
 int screenHeight = 720;
 float prevFrameTime;
 float deltaTime;
+glm::vec3 ambient_color;
 
-int main() {
+struct Material 
+{
+	float ka = 1.0;
+	float kd = 0.5;
+	float ks = 0.5;
+	float shininess = 128;
+}material;
+
+
+int main() 
+{
 	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
+
+	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	ew::Model model = ew::Model("assets/suzanne.obj");
+	ew::Transform modelTransform;
+
+	GLuint texture = ew::loadTexture("assets/brick_color.jpg");
+
+	camera.position = glm::vec3(0, 0, 5);
+	camera.target = glm::vec3(0, 0, 0);
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60;
+
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
-	while (!glfwWindowShouldClose(window)) {
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
+
+	// Game Loop
+	while (!glfwWindowShouldClose(window)) 
+	{
 		glfwPollEvents();
 
 		float time = (float)glfwGetTime();
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
-		//RENDER
-		glClearColor(0.6f,0.8f,0.92f,1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// Render
+		{
+			glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			cameraController.move(window, &camera, deltaTime);
+
+			modelTransform.rotation = glm::rotate(modelTransform.rotation, deltaTime, glm::vec3(0, 1, 0));
+
+			// Bind textures
+			glBindTextureUnit(0, texture);
+
+			// Set uniforms
+			shader.use();
+			shader.setInt("main_texture", 0);
+			shader.setVec3("eye_position", camera.position);
+			shader.setVec3("ambient_color", ambient_color);
+			shader.setFloat("material.ka", material.ka);
+			shader.setFloat("material.kd", material.kd);
+			shader.setFloat("material.ks", material.ks);
+			shader.setFloat("material.shininess", material.shininess);
+
+			shader.setMat4("model_matrix", modelTransform.modelMatrix());
+			shader.setMat4("projection_matrix", camera.projectionMatrix() * camera.viewMatrix());
+			model.draw();
+		}
 
 		drawUI();
 
 		glfwSwapBuffers(window);
 	}
+
 	printf("Shutting down...");
 }
 
-void drawUI() {
+void resetCamera()
+{
+	camera.position = glm::vec3(0, 0, 5.0f);
+	camera.target = glm::vec3(0);
+	cameraController.yaw = cameraController.pitch = 0;
+}
+
+void drawUI() 
+{
 	ImGui_ImplGlfw_NewFrame();
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui::NewFrame();
 
 	ImGui::Begin("Settings");
+
+	if (ImGui::Button("Reset Camera")) 
+	{
+		resetCamera();
+	}
+
+	if (ImGui::CollapsingHeader("Material")) 
+	{
+		ImGui::SliderFloat("AmbientK", &material.ka, 0.0f, 1.0f);
+		ImGui::SliderFloat("DiffuseK", &material.kd, 0.0f, 1.0f);
+		ImGui::SliderFloat("SpecularK", &material.ks, 0.0f, 1.0f);
+		ImGui::SliderFloat("Shininess", &material.shininess, 2.0f, 1024.0f);
+	}
+
 	ImGui::Text("Add Controls Here!");
 	ImGui::End();
 
@@ -67,7 +153,9 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 /// <param name="width">Window width</param>
 /// <param name="height">Window height</param>
 /// <returns>Returns window handle on success or null on fail</returns>
-GLFWwindow* initWindow(const char* title, int width, int height) {
+/// 
+GLFWwindow* initWindow(const char* title, int width, int height) 
+{
 	printf("Initializing...");
 	if (!glfwInit()) {
 		printf("GLFW failed to init!");
@@ -75,13 +163,16 @@ GLFWwindow* initWindow(const char* title, int width, int height) {
 	}
 
 	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
-	if (window == NULL) {
+	if (window == NULL) 
+	{
 		printf("GLFW failed to create window");
 		return nullptr;
 	}
+
 	glfwMakeContextCurrent(window);
 
-	if (!gladLoadGL(glfwGetProcAddress)) {
+	if (!gladLoadGL(glfwGetProcAddress)) 
+	{
 		printf("GLAD Failed to load GL headers");
 		return nullptr;
 	}
